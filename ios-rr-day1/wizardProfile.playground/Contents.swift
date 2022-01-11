71fd6f06ab136d6e9963f312c69dd10b3df5b894
Carlotta Chen <carlottachen92@gmail.com>
2022-01-11 11:16:18 -0800

    day1

---
+++ ios-rr-day1/wizardProfile.playground/Contents.swift
@@ -0,0 +1,20 @@
+
+let firstName = "Newton"
+let lastName = "Scamander"
+let birthYear = 1897
+var nationality = "English"
+let bloodStatus = "Pure-blood or Half-blood"
+let gender = "Male"
+var height = "6'1\""
+var weight = 161
+
+let harryPotterAppearance = false
+let fantasticBeastsAppearance = true
+let dumbledoresArmy = true
+let deathEater = false
+
+var HPImportance = "non-existent"
+var performance = 10
+var reason = "Main character and kind of quirky"
+
+print(firstName, lastName)
